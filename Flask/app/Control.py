de0331d77b8a37795769be9b511ae3f4da1c9c95
pagedhack <samu1@outlook.es>
2023-04-05 11:15:39 -0600

    Cambios

---
+++ Flask/app/Control.py
@@ -0,0 +1,8 @@
+control = [
+    {"fecha": 1 , "temperatura": 20, "humedad": 10, "Radiacion Solar": 0.400, "Radiacion uv": 4, "medicion agua": 5},
+    {"fecha": 2 , "temperatura": 25, "humedad": 16, "Radiacion Solar": 0.410, "Radiacion uv": 6, "medicion agua": 4},
+    {"fecha": 3 , "temperatura": 15, "humedad": 4, "Radiacion Solar": 0.320, "Radiacion uv": 3, "medicion agua": 6},
+    {"fecha": 4 , "temperatura": 27, "humedad": 8, "Radiacion Solar": 0.200, "Radiacion uv": 8, "medicion agua": 2},
+    {"fecha": 5 , "temperatura": 18, "humedad": 20, "Radiacion Solar": 0.320, "Radiacion uv": 2, "medicion agua": 6},
+    {"fecha": 6 , "temperatura": 24, "humedad": 15, "Radiacion Solar": 0.350, "Radiacion uv": 7, "medicion agua": 5},
+]
--- Flask/app/app.py
+++ Flask/app/app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+#importe de base de datos
+from Control import control
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/invernadero')
+def invernadero():
+    return jsonify({"Control Invernadero" + "- semana 1" : control})
+
+@app.route('/invernadero/<int:control_fecha>')
+def getDatos(control_fecha):
+    datos = [dato for dato in control if dato['fecha'] == control_fecha]
+    return jsonify(datos[0])
+
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
